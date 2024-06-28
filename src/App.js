457d105ad2481317269a0343ca8bcee4c570594f
Ирина Болганова <youremail@example.com>
2024-06-28 22:50:23 +0300

    Changes have been made

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { data } from './data';
 import './App.css';
 
 function App() {
+
+const [person, setPerson] = useState(0);
+const {id, name, height, birthDate, image} = data[person];
+
+const previosPerson = () => {
+  setPerson((person => {
+    person --;
+    if (person < 0) {
+      return data.length-1;
+    }
+    return person;
+  }))
+
+}
+
+const nextPerson = () => {
+  setPerson((person => {
+    person ++;
+    if (person > data.length - 1) {
+      person = 0;
+    }
+    return person;
+  }))
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+  <div className='container'> 
+      <h1>10 корейских актеров, которых обожают сами корейцы</h1>
+  </div>
+    <div className="container">
+      <img src={image} width="300px" alt="People"/>
+    </div>
+
+      <div className="container">
+        <h2>{id} - {name}</h2>
+      </div>
+
+      <div className="container">
+        <h2>{height}</h2>
+      </div>
+
+      <div className="container">
+        <h2>{birthDate}</h2>
+      </div>
+
+    <div className="btn">
+      <button onClick={previosPerson}>《 </button>
+      <button onClick={nextPerson}> 》</button>
+    </div>
+
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,81 @@
+export const data = [
+
+    {
+    id: 1,
+    name: "Li Min-ho",
+    height: "1.87 м",
+    birthDate: "22 июня, 1987",
+    image: "https://avatars.mds.yandex.net/get-kinopoisk-image/10592371/3ff9c7e8-82dc-4739-a9bf-41e522ea8727/600x900"
+    },
+
+    {
+    id: 2,
+    name: "Kim Soo-hyeon",
+    height: "1.8 м",
+    birthDate: "16 февраля, 1988",
+    image: "https://avatars.mds.yandex.net/get-kinopoisk-image/4303601/1eee78a1-6114-4b2e-8ce2-2bf4957723a5/600x900"
+    },
+
+    {
+    id: 3,
+    name: "So Ji-seop",
+    height: "1.82 м",
+    birthDate: "4 ноября, 1977",
+    image: "https://avatars.mds.yandex.net/get-kinopoisk-image/9784475/ad2f6c22-943d-4167-bf01-e25f739be705/600x900"
+    },
+
+    {
+    id: 4,
+    name: "Ji Chang-wook",
+    height: "1.8 м",
+    birthDate: "5 июля, 1987",
+    image: "https://avatars.mds.yandex.net/get-kinopoisk-image/1777765/e055616f-203d-40d3-92b8-4b33931ae557/600x900"
+    },
+
+    {
+    id: 5,
+    name: "Lee Jong-seok",
+    height: "1.86 м",
+    birthDate: "14 сентября, 1989 ",
+    image: "https://avatars.mds.yandex.net/get-kinopoisk-image/1599028/90d2c635-07f5-4cf3-8403-64779b6bc3d9/600x900"
+    },
+
+    {
+    id: 6,
+    name: "Park Bo-geom",
+    height: "1.82 м",
+    birthDate: "16 июня, 1993 ",
+    image: "https://avatars.mds.yandex.net/get-kinopoisk-image/1704946/9804ff99-028d-46ce-8455-c0635d3fef03/600x900"
+    },
+
+    {
+    id: 7,
+    name: "Kim Woo-bin",
+    height: "1.88 м",
+    birthDate: "16 июля, 1989",
+    image: "https://avatars.mds.yandex.net/get-kinopoisk-image/4303601/d49c4a81-3d5c-4522-94a3-567c358dbf5b/600x900"
+    },
+
+    {
+    id: 8,
+    name: "Hyeon Bin",
+    height: "1.85 м",
+    birthDate: "25 сентября, 1982 ",
+    image: "https://avatars.mds.yandex.net/get-kinopoisk-image/1629390/b31f3a83-a184-4f69-a142-f44d76384dfa/600x900"
+    },
+
+    {
+    id: 9,
+    name: "Joo Ji-hoon",
+    height: "1.88 м",
+    birthDate: "16 мая, 1982",
+    image: "https://avatars.mds.yandex.net/get-kinopoisk-image/1600647/9b4cc771-1599-4116-b115-40f670b57492/600x900"
+    },
+    {
+    id: 10,
+    name: "Won Bin",
+    height: "1.78 м",
+    birthDate: "10 ноября, 1977",
+    image: "https://avatars.mds.yandex.net/get-kinopoisk-image/1629390/0ec0fc59-62ae-4fbb-82c0-838d182cfe9d/600x900"
+    }
+]
